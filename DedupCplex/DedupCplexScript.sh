323f2a2816f5377a8f4422dd9f8961f1fd5cf099
Nadav-Levintov <DSP155@Gmail.com>
2018-12-23 22:02:57 +0200

    Script update

--- DedupCplex/DedupCplexScript.sh
+++ DedupCplex/DedupCplexScript.sh
@@ -9,6 +9,7 @@ K="0"
 EPSILON="0"
 OUTFILE=""
 KBTGB=$MB
+GZ=".gz"
 
 function log_run {
 	TYPE=`cat ${FILE}${CSV} | grep 'Output type:' | cut -d ":" -f 2 | tr -d '[:space:]'`
@@ -56,7 +57,7 @@ function log_run {
 		FSN="$(($FSN+0))"
 	fi
 	NUMFS="$(($FSN-$FS1))"
-	echo -n "${FILE}${CSV},${TYPE},${DEPTH},${FS1},${FSN},${NUMFS},${ATYPE},${FILES}," >> runs.csv
+	echo -n "${FILE}${CSV},${TYPE},${DEPTH},${FS1},${FSN},${NUMFS},${ATYPE},${FILES}," >> ${SCRIPT_DIR}/runs.csv
 	RESULTFILE=`ls | grep 'result'`
 	if [ "${RESULTFILE}" == "" ]; then
 		PHYSICAL="0"
@@ -85,7 +86,7 @@ function log_run {
 		SOLVETIME=`cat ${RESULTFILE} | grep 'Solve Time' | cut -d ":" -f 2 | tr -d '[:space:]'`
 	fi	
 		
-	echo -n "${PHYSICAL},${BLOCKS},${TOTAL},${KBYTES},${EPSILONBYTES},${MOVEDBYTES},${COPIEDBYTES},${MOVEDFILES},${K},${EPSILON},${MOVEDPERCENT},${COPIEDPERCENT},${INPUTTIME},${SOLVETIME}" >> runs.csv
+	echo -n "${PHYSICAL},${BLOCKS},${TOTAL},${KBYTES},${EPSILONBYTES},${MOVEDBYTES},${COPIEDBYTES},${MOVEDFILES},${K},${EPSILON},${MOVEDPERCENT},${COPIEDPERCENT},${INPUTTIME},${SOLVETIME}" >> ${SCRIPT_DIR}/runs.csv
 	calc_time_and_ram
 	rm ${RESULTFILE}
 }
@@ -94,16 +95,55 @@ function calc_time_and_ram {
 	#T=`cat time.csv | cut -d ',' -f 1`
 	MEM=`cat time.csv | cut -d ',' -f 2`
 	MEM=`printf "%.2f\n" "$(bc -l <<< ${MEM}/${KBTGB} )"`
-	echo ",${MEM}" >> runs.csv
+	echo ",${MEM}" >> ${SCRIPT_DIR}/runs.csv
 }
 
 function run_dedup {
-	OUTFILE="${FILE}_K${K}_E${EPSILON}"
+	OUTFILE="${FILE}_K${K}_E${EPSILON}_R${RUN}"
 	#750m = 12.5h
-    /usr/bin/time -f "%e,%M" -o time.csv timeout --preserve-status 750m ./DedupCplex ${FILE}${CSV} ${K}'%' ${EPSILON}'%' > "${FILE}_K${K}_E${EPSILON}${LOG}" 
+    /usr/bin/time -f "%e,%M" -o time.csv timeout --preserve-status 750m ${SCRIPT_DIR}/DedupCplex ${FILE}${CSV} ${K}'%' ${EPSILON}'%' > "${FILE}_K${K}_E${EPSILON}_R${RUN}${LOG}" 
 	log_run
 	rm -rf time.csv
 	mv lpex1.lp ${OUTFILE}.lp
+}
+
+function run_dedup_code {
+	for FILE in $( ls -Sr | grep ".csv" | cut -f 1 -d . | grep -v "runs" ); do
+
+		rm -rf ${FILE}
+		rm -rf time.csv
+		#mkdir ${FILE}
+
+		if [[ -a ${FILE}${CSV}${GZ} ]]; then
+			gunzip ${FILE}${CSV}${GZ}
+		fi
+		
+		echo "Current file: ${FILE}"
+
+		for K in 10 20 30 40 50; do
+			for EPSILON in 0.5 1 2 5; do
+				for RUN in 1 2 3; do
+					run_dedup
+				done
+			done
+		done
+		#mv *${FILE}*.* ${FILE}
+		rm ${FILE}${CSV}
+	done
+}
+
+function traverse_directories {
+	for DIR in $( ls ); do
+	if [[ -d ${DIR} ]]; then
+		cd ${DIR}
+			echo "Going into dir: ${DIR}"
+
+			traverse_directories
+			echo "Exiting from dir"
+		cd ..
+	fi
+	done
+	run_dedup_code
 }
 
 rm -rf time.csv
@@ -116,14 +156,7 @@ fi
 echo "#`date`" >> runs.csv
 echo "Input file, Type, Depth, FS1, FSN, NumFS, Aggregate type, Num Logical, Num Physical, Num Blocks, Num Containers , Total Bytes, K Bytes, Epsilon Bytes, Moved bytes, Copied Bytes, Moved Files, K, Epsilon, Moved, Copied, Process time [Seconds], Solver time [Seconds], RAM[GB]" >> runs.csv
 
-for FILE in $( ls -Sr | grep ".csv" | cut -f 1 -d . | grep -v "runs" ); do
-    rm -rf ${FILE}
-	rm -rf time.csv
-	mkdir ${FILE}
-	for K in 10 20 30 40 50; do
-		for EPSILON in 0.5 1 2 5; do
-			run_dedup
-		done
-	done
-    mv *${FILE}*.* ${FILE}
-done
+SCRIPT_DIR=`pwd`
+
+traverse_directories
+echo "All Done"
