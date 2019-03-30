d905f36fb5ba50efc27b18ca5e41f7b04b3e26b1
Nadav-Levintov <DSP155@Gmail.com>
2019-03-30 20:55:26 +0300

    Fix script

--- DedupCplex/FixCsvScript.sh
+++ DedupCplex/FixCsvScript.sh
@@ -11,32 +11,33 @@ OUTFILE=""
 KBTGB=$MB
 GZ=".gz"
 FILENAME=""
-function getMinSysNum{
-	MINSYS=`cat $FILENAME | grep "F" | cut -f3 -d"," | cut -f1 -d"_" | sort | tail -1`
+
+function getMinSysNum {
+	MINSYS=`cat ${FILEPATH} | grep "F" | cut -f3 -d"," | cut -f1 -d"_" | sort | tail -1`
 }
 
-function getMaxSysNum{
-	MAXSYS=`cat $FILENAME | grep "F" | cut -f3 -d"," | cut -f1 -d"_" | sort | head -1`
+function getMaxSysNum {
+	MAXSYS=`cat ${FILEPATH} | grep "F" | cut -f3 -d"," | cut -f1 -d"_" | sort | head -1`
 }
 
-function getContainers{
-	CONTAINERS=`cat $FILENAME | grep "#" | grep "containers" -i | grep "num" -i | cut -f2 -d":"`
+function getContainers {
+	CONTAINERS=`cat ${FILEPATH} | grep "#" | grep "containers" -i | grep "num" -i | cut -f2 -d":"`
 }
 
-function getBlocks{
-	BLOCKS=`cat $FILENAME | grep "#" | grep "blocks" -i | grep "num" -i | cut -f2 -d":"`
+function getBlocks {
+	BLOCKS=`cat ${FILEPATH} | grep "#" | grep "blocks" -i | grep "num" -i | cut -f2 -d":"`
 }
 
-function getFiles{
-	FILES=`cat $FILENAME | grep "#" | grep "files" -i | grep "num" -i | cut -f2 -d":"`
+function getFiles {
+	FILES=`cat ${FILEPATH} | grep "#" | grep "files" -i | grep "num" -i | cut -f2 -d":"`
 }
 
-function getNumSystem{
-	SYSTEM=`cat $FILENAME  | grep "F" | cut -f3 -d"," | cut -f1 -d"_" | uniq | wc -l`
+function getNumSystem {
+	SYSTEM=`cat ${FILEPATH}  | grep "F" | cut -f3 -d"," | cut -f1 -d"_" | uniq | wc -l`
 }
 
-function physicalOrLogical{
-	if [[ $FILENAME =~ .*_b_.* ]] || [[ $FILENAME =~ .*_B_.* ]]
+function physicalOrLogical {
+	if [[ ${FILEPATH} =~ .*_b_.* ]] || [[ ${FILEPATH} =~ .*_B_.* ]]
 	then
 		LOGICAL=$FILES
 		PHYSICAL="0"
@@ -45,5 +46,51 @@ function physicalOrLogical{
 		LOGICAL="0"
 	fi
 }
+
+RUNS_FILE=$1
+if [  ! -f ${RUNS_FILE}.csv ]
+then
+	echo "File ${RUNS_FILE}.csv doesn't exist"
+	exit
+fi
+
+echo "#`date`" > ${RUNS_FILE}_fixed.csv
+echo "Input file, Type, Depth, FS1, FSN, NumFS, Aggregate type, Num Logical, Num Physical, Num Blocks, Num Containers , Total Bytes, K Bytes, Epsilon Bytes, Moved bytes, Copied Bytes, Moved Files, K, Epsilon, Moved, Copied, Process time [Seconds], Solver time [Seconds], RAM[GB]" >> ${RUNS_FILE}_fixed.csv
+
+while IFS="," read -r -a line
+do
+	# Disregard comments or header lines
+	if [[ ${line[0]} == *[#]* ]] || [[ ${line[0]} == "Input file" ]]
+	then
+		continue
+	fi
+
+	FILENAME=${line[0]}
+	echo "Fixing ${FILENAME}"
+	FILEPATH=`find . -name ${FILENAME}*`
+	if [[ -a ${FILEPATH} ]]; then
+		gunzip ${FILEPATH}
+		FILEPATH=`find . -name ${FILENAME}*`
+	fi
+
+	getMinSysNum
+	line[3]=${MINSYS}
+	getMaxSysNum
+	line[4]=${MAXSYS}
+	getNumSystem
+	line[5]=${SYSTEM}
+	getFiles
+	physicalOrLogical
+	line[7]=${PHYSICAL}
+	line[8]=${LOGICAL}
+	getContainers
+	line[9]=${CONTAINERS}
+	getBlocks
+	line[10]=${BLOCKS}
+
+	IFS=","; echo "${line[@]:0:24}" >> ${RUNS_FILE}_fixed.csv
+
+	gzip ${FILEPATH}
+done <"${RUNS_FILE}.csv"
 
 
